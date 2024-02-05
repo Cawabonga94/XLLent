bc6da8433cb6fa05257a8f5500c508f059a96826
ITMASTER\V.Piccioni <v.piccioni@reactive-almaviva.it>
2024-02-05 16:21:50 +0100

    XLLent

--- my_plugin.py
+++ my_plugin.py
@@ -1,31 +1,15 @@
-from cat.mad_hatter.decorators import tool, hook, plugin
-from pydantic import BaseModel
-from datetime import datetime, date
+from cat.mad_hatter.decorators import hook
 
-class MySettings(BaseModel):
-    required_int: int
-    optional_int: int = 69
-    required_str: str
-    optional_str: str = "meow"
-    required_date: date
-    optional_date: date = 1679616000
+from .parsers import XLSXExcelParser, XLSExcelParser
 
-@plugin
-def settings_model():
-    return MySettings
-
-@tool
-def get_the_day(tool_input, cat):
-    """Get the day of the week. Input is always None."""
-
-    dt = datetime.now()
-
-    return dt.strftime('%A')
 
 @hook
-def before_cat_sends_message(message, cat):
+def rabbithole_instantiates_parsers(file_handlers: dict, cat) -> dict:
 
-    prompt = f'Rephrase the following sentence in a grumpy way: {message["content"]}'
-    message["content"] = cat.llm(prompt)
-
-    return message
+    new_handlers = {
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": XLSXExcelParser(),
+        "application/vnd.ms-excel": XLSExcelParser()
+    }
+     
+    file_handlers = file_handlers | new_handlers
+    return file_handlers
